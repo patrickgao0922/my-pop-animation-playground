8351ad4a618d8f090d4a665f2d2c071f9b089b66
Patrick Gao <patrickgao0922@gmail.com>
2017-02-15 10:27:48 +1100

    Initial Commit

---
+++ my-pop-animation-playground/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  my-pop-animation-playground
+//
+//  Created by Patrick Gao on 15/2/17.
+//  Copyright © 2017 com.patrickgao. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
